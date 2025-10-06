f419c47155ba2b714be1ee90d66f9662148d2d2f
GideonnAI <gideonsatoru@gmail.com>
2025-10-06 14:55:25 +0700

    Create help.py

---
+++ Gemini/modules/help.py
@@ -0,0 +1,15 @@
+from telethon import events, Button
+from Gemini import bot
+
+@bot.on(events.NewMessage(pattern="[!?/]help ?(.*)"))
+async def help(event):
+    if event.is_group:
+       await event.reply("Tag saya lalu ketik apa saja untuk memulai percakapan")
+       return
+
+    await event.reply("Silahkan ketik apa saja untuk memulai percakapan!", buttons=(Button.inline("Kembali", data="home")))
+
+
+@bot.on(events.callbackquery.CallbackQuery(data="help"))
+async def chelp(event):
+     await event.edit(HELP_TEXT, buttons=(Button.inline("Kembali", data="home")))
