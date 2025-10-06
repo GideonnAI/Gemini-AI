3e6a5262dc141c172517d16e3a2130a7144e0667
GideonnAI <gideonsatoru@gmail.com>
2025-10-06 18:19:28 +0700

    Update chat.py

--- Gemini/modules/chat.py
+++ Gemini/modules/chat.py
@@ -5,7 +5,7 @@ from Gemini import bot
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", None)
 
 async def ask_gemini(prompt, api_key):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
     
     data = {
         "contents": [{
