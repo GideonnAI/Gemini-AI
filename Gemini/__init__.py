4d6987527659edb46f92d585223c487f43751ea0
GideonnAI <gideonsatoru@gmail.com>
2025-10-06 14:26:37 +0700

    Create __init__.py

---
+++ Gemini/__init__.py
@@ -0,0 +1,14 @@
+import os, logging
+from telethon import TelegramClient
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+TOKEN = os.environ.get("TOKEN", None)
+
+
+bot = TelegramClient(
+        "tgbot",
+        api_id=6,
+        api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e"
+        ).start(bot_token=TOKEN)
