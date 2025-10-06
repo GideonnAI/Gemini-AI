130e7ffe4a9072c3c924fadbe13afc5b21cf74ce
GideonnAI <gideonsatoru@gmail.com>
2025-10-06 18:36:21 +0700

    Update help.py

--- Gemini/modules/help.py
+++ Gemini/modules/help.py
@@ -11,14 +11,14 @@ Chat Pribadi: Silahkan ketik apa saja untuk memulai percakapan!
 @bot.on(events.NewMessage(pattern="[!?/]help ?(.*)"))
 async def help(event):
     if event.is_group:
-       await event.reply(HELP_TEXT.format(bot.get_me().username))
+       await event.reply(HELP_TEXT.format(bot_username=bot.get_me().username))
        return
 
-    await event.respond(HELP_TEXT.format(bot.get_me().username), buttons=[[(Button.inline("Kembali", data="home"))]])
+    await event.respond(HELP_TEXT.format(bot_username=bot.get_me().username), buttons=[[Button.inline("Kembali", data="home")]])
 
 
 @bot.on(events.callbackquery.CallbackQuery(data="help"))
 async def chelp(event):
-     await event.edit(HELP_TEXT.format(bot.get_me().username), 
+     await event.edit(HELP_TEXT.format(bot_username=bot.get_me().username), 
      buttons=[
-     [(Button.inline("Kembali", data="home"))]])
+     [Button.inline("Kembali", data="home")]])
