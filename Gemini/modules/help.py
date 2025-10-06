955f9477c187b6d91980eb33eecd6a73abd3b1a6
GideonnAI <gideonsatoru@gmail.com>
2025-10-06 17:19:01 +0700

    Update help.py

--- Gemini/modules/help.py
+++ Gemini/modules/help.py
@@ -1,8 +1,7 @@
 from telethon import events, Button
 from Gemini import bot
 
-bot_username = bot.get_me().username
-HELP_TEXT = f"""
+HELP_TEXT = """
 Di Grup: Tag saya lalu ketik apa saja untuk memulai percakapan
 Contoh: @{bot_username} Apa itu AI?
 
@@ -12,12 +11,12 @@ Chat Pribadi: Silahkan ketik apa saja untuk memulai percakapan!
 @bot.on(events.NewMessage(pattern="[!?/]help ?(.*)"))
 async def help(event):
     if event.is_group:
-       await event.reply(HELP_TEXT)
+       await event.reply(HELP_TEXT.format(bot.get_me().username))
        return
 
-    await event.respond(HELP_TEXT, buttons=(Button.inline("Kembali", data="home")))
+    await event.respond(HELP_TEXT.format(bot.get_me().username)), buttons=(Button.inline("Kembali", data="home")))
 
 
 @bot.on(events.callbackquery.CallbackQuery(data="help"))
 async def chelp(event):
-     await event.edit(HELP_TEXT, buttons=(Button.inline("Kembali", data="home")))
+     await event.edit(HELP_TEXT.format(bot.get_me().username)), buttons=(Button.inline("Kembali", data="home")))
